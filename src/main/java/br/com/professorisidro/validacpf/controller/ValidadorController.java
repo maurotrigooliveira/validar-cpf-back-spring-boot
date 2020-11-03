78f52f0bbd4b358630d2956ee28502ca1a14fbe3
MAURO TRIGO DE OLIVEIRA <003765963@itaud.des.ihf>
2020-11-03 17:39:19 -0300

    acerto do nome

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -18,5 +18,9 @@ public class ValidadorController {
 		}
 		 return "{\"status\": \"invalido\"}";
 	}
-
+	@GetMapping("/")
+	public String index() {
+		
+		 return "{\"mensagem\": \"seja bem vindo a api do Mauro\"}";
+	}
 }
